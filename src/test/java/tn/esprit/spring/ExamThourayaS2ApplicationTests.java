3bde91982092bc3cd843dab6ed4e693697e6eec1
Mohamed Ali <42294664+medAli-ai@users.noreply.github.com>
2022-12-13 04:37:51 +0100

    Update ExamThourayaS2ApplicationTests.java

--- src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
+++ src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
@@ -1,1 +1,35 @@
+package tn.esprit.spring;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Voyageur;
+import tn.esprit.spring.repository.VoyageurRepository;
+import tn.esprit.spring.services.VoyageurServiceImpl;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class ExamThourayaS2ApplicationTests {
+  
+  @Mock
+	VoyageurRepository  voyageurRepository ;
+	
+	@InjectMocks
+	VoyageurServiceImpl  voyageurService;
+
+	@Test
+	void contextLoads() {
+    
+    SecteurActivite secteur = new SecteurActivite(1L,"123","commercial",null);
+		secteur.setIdSecteurActivite(1L);
+		
+		
+		Mockito.when(SecteurRepository.findById(1L)).thenReturn(Optional.of(secteur));
+		SecteurService.retrieveSecteurActivite(1L);
+		Assertions.assertNotNull(secteur);
+		
+		System.out.println(secteur);
+		System.out.println("Test 1 : ID retrival - work !");
+	}
+
+}
