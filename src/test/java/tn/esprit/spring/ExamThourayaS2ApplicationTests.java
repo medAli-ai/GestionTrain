6dcf78b14be1821fe863fd7be43c1ead1580ba47
Mohamed Ali <42294664+medAli-ai@users.noreply.github.com>
2022-12-13 04:43:19 +0100

    Update ExamThourayaS2ApplicationTests.java

--- src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
+++ src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
@@ -2,6 +2,23 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Voyageur;
 import tn.esprit.spring.repository.VoyageurRepository;
 import tn.esprit.spring.services.VoyageurServiceImpl;
@@ -20,15 +37,15 @@ class ExamThourayaS2ApplicationTests {
 	@Test
 	void contextLoads() {
     
-    SecteurActivite secteur = new SecteurActivite(1L,"123","commercial",null);
-		secteur.setIdSecteurActivite(1L);
+    		Voyageur voyageur = new Voyageur(1L,"123","Dali Bouchhioua");
+		secteur.setIdVoyageur(1L);
 		
 		
-		Mockito.when(SecteurRepository.findById(1L)).thenReturn(Optional.of(secteur));
-		SecteurService.retrieveSecteurActivite(1L);
-		Assertions.assertNotNull(secteur);
+		Mockito.when(voyageurRepository.findById(1L)).thenReturn(Optional.of(voyageur));
+		voyageurService.recupererVoyageParId(1L);
+		Assertions.assertNotNull(voyageur);
 		
-		System.out.println(secteur);
+		System.out.println(voyageur);
 		System.out.println("Test 1 : ID retrival - work !");
 	}
 
