6159fbe21d0a12b7eb093f85f6c42d29d060d989
Mohamed Ali <42294664+medAli-ai@users.noreply.github.com>
2022-12-13 04:55:39 +0100

    Update ExamThourayaS2ApplicationTests.java

--- src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
+++ src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
@@ -1,8 +1,5 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -13,11 +10,9 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Voyageur;
 import tn.esprit.spring.entities.Voyage;
@@ -26,7 +21,6 @@ import tn.esprit.spring.services.VoyageurServiceImpl;
 
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 class ExamThourayaS2ApplicationTests {
   
   @Mock
@@ -45,7 +39,6 @@ class ExamThourayaS2ApplicationTests {
 		
 		Mockito.when(voyageurRepository.findById(1L)).thenReturn(Optional.of(voyageur));
 		voyageurService.recupererVoyageParId(1L);
-		Assertions.assertNotNull(voyageur);
 		
 		System.out.println(voyageur);
 		System.out.println("Test 1 : ID retrival - work !");
